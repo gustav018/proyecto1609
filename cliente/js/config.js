52c8129f00109a3751607058d5c2ffba49473ca9
Gustavo Fleitas <gustavofleitas18@gmail.com>
2024-11-25 23:23:20 -0300

    Update config.js

--- cliente/js/config.js
+++ cliente/js/config.js
@@ -1,12 +1,12 @@
    // Configuración de Firebase
    var firebaseConfig = {
-    apiKey: "AIzaSyDZSNRXlFKCVovRssMYfgI0VSnUXsH1lIw",
-    authDomain: "ppy2023.firebaseapp.com",
-    projectId: "ppy2023",
-    storageBucket: "ppy2023.appspot.com",
-    messagingSenderId: "16887769964",
-    appId: "1:16887769964:web:958c612a9ce945ff71a149",
-    measurementId: "G-1LX5G8FMKB"
+   apiKey: "AIzaSyCk_kXgL5JkMDcEGm6ga-weOo6PozI6wZA",
+  authDomain: "ppy2023.firebaseapp.com",
+  projectId: "ppy2023",
+  storageBucket: "ppy2023.appspot.com",
+  messagingSenderId: "16887769964",
+  appId: "1:16887769964:web:354e932daa55ee7b71a149",
+  measurementId: "G-54M0GN3FK3"
 };
   // Inicializar Firebase
   firebase.initializeApp(firebaseConfig);
